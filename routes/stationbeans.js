cbb8e917578b45d16fa3c3faebbbee382c003d63
talentedbernhard@gmail.com <talentedbernhard@gmail.com>
2020-02-07 20:53:47 +0100

    api call

--- routes/stationbeans.js
+++ routes/stationbeans.js
@@ -1,7 +1,15 @@
 const express = require('express')
 const router = express.Router()
+const request = require('request');
+
+
 
 router.get('/',  (req, res) => {
+    request('http://citibikenyc.com/stations/json', function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            console.log(body) // Print the google web page.
+        }
+    })
     res.send('We are on Login page')
 })
 
