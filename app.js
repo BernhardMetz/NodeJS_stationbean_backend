16b32d6f131cb3595e0a30115c26f447a783a20d
talentedbernhard@gmail.com <talentedbernhard@gmail.com>
2020-02-07 20:47:36 +0100

    env was updated

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ const stationbeansRoute = require('./routes/stationbeans')
 const app = express()
 app.use(cors())
 app.use(bodyParser.json())
-app.use('/api/getstaionbeans', stationbeansRoute)
-
+app.use('/api/getstationbeans', stationbeansRoute)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT)
+console.log(`Server is running on port ${PORT}`)
